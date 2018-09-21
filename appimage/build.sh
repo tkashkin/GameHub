edea459723172a6b98963c4f00f8301b95644d18
tkashkin <tkashkin@gmail.com>
2018-09-21 06:42:41 +0300

    Update AppVeyor config

--- appimage/build.sh
+++ appimage/build.sh
@@ -71,6 +71,7 @@ build_deb()
 	cd "$_ROOT"
 	export DEB_BUILD_OPTIONS="nostrip,nocheck"
 	dpkg-buildpackage -b -us -uc
+	mkdir -p "build/$_BUILD_IMAGE"
 	mv ../*.deb "build/$_BUILD_IMAGE/GameHub-$_VERSION-amd64.deb"
 	cd "$_ROOT"
 }
