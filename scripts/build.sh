823d832201739ccfe82118bb9a7cac619c1acd0a
tkashkin <tkashkin@gmail.com>
2018-10-06 11:48:52 +0300

    Try to start gpg-agent

--- scripts/build.sh
+++ scripts/build.sh
@@ -69,7 +69,7 @@ import_keys()
 		./appveyor-tools/secure-file -decrypt "$_SCRIPTROOT/launchpad/key_sec.gpg.enc" -secret $keys_enc_secret
 		./appveyor-tools/secure-file -decrypt "$_SCRIPTROOT/launchpad/passphrase.enc" -secret $keys_enc_secret
 		echo "\nallow-loopback-pinentry\n" >> ~/.gnupg/gpg-agent.conf
-		kill -9 gpg-agent
+		killall gpg-agent
 		source <(gpg-agent --daemon)
 		gpg --pinentry-mode loopback --import "$_SCRIPTROOT/launchpad/key_pub.gpg"
 		gpg --pinentry-mode loopback --allow-secret-key-import --import "$_SCRIPTROOT/launchpad/key_sec.gpg"
@@ -107,11 +107,13 @@ build_deb()
 	export DEB_BUILD_OPTIONS="nostrip nocheck"
 	dpkg-buildpackage -us -uc
 	if [[ -e "$_SCRIPTROOT/launchpad/passphrase" && -n "$keys_enc_secret" ]]; then
-		kill -9 gpg-agent
+		set +e
+		killall gpg-agent
 		source <(gpg-agent --daemon)
 		debsign -p"gpg --pinentry-mode loopback --passphrase-file $_SCRIPTROOT/launchpad/passphrase --batch" -S -k2744E6BAF20BA10AAE92253F20442B9273408FF9 ../*.changes
 		rm "$_SCRIPTROOT/launchpad/passphrase"
 		dput -c "$_SCRIPTROOT/launchpad/dput.cf" "gamehub_$_DEB_TARGET_DISTRO" ../*.changes
+		set -e
 	fi
 	mkdir -p "build/$_BUILD_IMAGE"
 	cp ../*.deb "build/$_BUILD_IMAGE/GameHub-$_VERSION-amd64.deb"
